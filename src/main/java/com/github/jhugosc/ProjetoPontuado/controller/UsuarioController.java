6df11ac41adf312d483fd4d54816e2ba37c1d787
Hugo Santana <sjoaohugo@gmail.com>
2025-04-30 11:06:27 -0300

    Backend finalizado

--- src/main/java/com/github/jhugosc/ProjetoPontuado/controller/UsuarioController.java
+++ src/main/java/com/github/jhugosc/ProjetoPontuado/controller/UsuarioController.java
@@ -1,6 +1,24 @@
 package com.github.jhugosc.ProjetoPontuado.controller;
 
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.github.jhugosc.ProjetoPontuado.model.UsuarioModel;
+
+@Controller
+@RequestMapping("/projeto")
 public class UsuarioController {
     
+    @GetMapping("/getAll")
+    public List<UsuarioModel> getAllUsuarioModel(){
+        return List.of(
+        new UsuarioModel(1, "Carlos", "carlos@email.com", "(71)12340567"),
+        new UsuarioModel(2, "João", "joao@email.com", "(71)23567897"),
+        new UsuarioModel(3, "Pedro", "pedro@email.com", "(71)23574567"),
+        new UsuarioModel(4, "Lucas", "lucas@email.com", "(71)84345674"));
+    }
 
 }
