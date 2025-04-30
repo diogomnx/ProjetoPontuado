ff50818ee60836b85b175060c6d4a6ed78f9dba1
Hugo Santana <sjoaohugo@gmail.com>
2025-04-30 11:59:56 -0300

    Projeto finalizado

---
+++ src/main/java/com/github/jhugosc/ProjetoPontuado/controller/HtmlController.java
@@ -0,0 +1,22 @@
+package com.github.jhugosc.ProjetoPontuado.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HtmlController {
+@RequestMapping("/projeto")
+public String index() {
+    return "index";
+}
+
+@RequestMapping("/projeto/home")
+public String home() {
+    return "home";
+}
+
+@RequestMapping("/projeto/getAll")
+public String infos() {
+    return "infos";
+}
+}
