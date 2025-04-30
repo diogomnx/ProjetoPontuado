a39dcab3d65f58a64b990cb48eefe48f2ec5f13a
Hugo Santana <sjoaohugo@gmail.com>
2025-04-30 10:24:49 -0300

    Primeiro commit

---
+++ src/main/java/com/github/jhugosc/ProjetoPontuado/ProjetoPontuadoApplication.java
@@ -0,0 +1,13 @@
+package com.github.jhugosc.ProjetoPontuado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPontuadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPontuadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jhugosc/ProjetoPontuado/ProjetoPontuadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jhugosc.ProjetoPontuado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPontuadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
